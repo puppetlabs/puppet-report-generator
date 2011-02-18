1c4c3b9262e5550e23e22f178bdf37bfd80f1689
Nick Lewis <nick@puppetlabs.com>
2011-02-18 11:42:53 -0800

    Generates random hostnames now

--- generate_report.rb
+++ generate_report.rb
@@ -2,6 +2,25 @@
 
 require 'erb'
 require 'yaml'
+require 'rubygems'
+require 'active_support'
+
+def generate_hostname
+  if @words.nil?
+    @words = []
+    File.open("/usr/share/dict/words").each_line do |line|
+      @words << line.chomp
+    end
+    @words.delete_if {|word| word.length < 6 or word.squeeze != word or word.downcase != word}
+  end
+
+  @domain ||= @words.random_element
+  @ext ||= ["net", "org", "com", "co.uk"].random_element
+
+  host = @domain
+  host = @words.random_element until host != @domain
+  "#{host}.#{@domain}.#{@ext}"
+end
 
 report_template = <<REPORT_TEMPLATE
 --- !ruby/object:Puppet::Transaction::Report
@@ -17,15 +36,16 @@ report_template = <<REPORT_TEMPLATE
   time: <%= params[:time] %>
 REPORT_TEMPLATE
 
-
-$ARGV.each_with_index do |hostname,index|
+5.times do
+  hostname = generate_hostname
+  puts hostname
   params = {
     :configuration_version => Time.now.to_i,
     :host => hostname,
     :status => "failed",
     :time => Time.now.to_yaml.chomp.gsub('--- ',''),
   }
-  File.open("yaml/#{index}.yaml","w") do |f|
+  File.open("yaml/#{hostname}.yaml","w") do |f|
     f.write ERB.new(report_template).result(params.send(:binding))
   end
 end
