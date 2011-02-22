a7a33998cb657b5db501ee87040cc1d44230f802
Nick Lewis <nick@puppetlabs.com>
2011-02-22 13:54:01 -0800

    Restoring functionality of creating multiple reports to populate_dashboard

--- populate_dashboard.rb
+++ populate_dashboard.rb
@@ -5,17 +5,18 @@ require 'rubygems'
 
 rakefile_if_specified = "-f #{ARGV.first}" unless ARGV.empty?
 
-#100.times do
-report = DataGenerator.generate_report
-File.open("yaml/#{report.host}.yaml","w") do |f|
-	f.print report.to_yaml
+100.times do
+  report = DataGenerator.generate_report
+  File.open("yaml/#{report.host}.yaml","w") do |f|
+    f.print report.to_yaml
+  end
 end
-#end
-#
-#puts "Importing reports"
-puts "rake #{rakefile_if_specified} reports:import REPORT_DIR=yaml"
-#
-#10.times do
-#  puts `rake #{rakefile_if_specified} node:add name=#{generate_hostname}`
-#end
+
+puts "Importing reports"
+`rake #{rakefile_if_specified} reports:import REPORT_DIR=yaml`
+
+puts "Creating unresponsive nodes"
+10.times do
+  puts `rake #{rakefile_if_specified} node:add name=#{generate_hostname}`
+end
 
