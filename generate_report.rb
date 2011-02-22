421cb63c7ead17b5cea103d3be38d07571913fca
Nick Lewis <nick@puppetlabs.com>
2011-02-22 13:42:58 -0800

    Refactored report generation to use Puppet-like objects

--- generate_report.rb
+++ generate_report.rb
@@ -6,92 +6,121 @@ require 'ostruct'
 require 'rubygems'
 require 'active_support'
 
-def generate_hostname
-  if @words.nil?
-    @words = []
-    File.open("/usr/share/dict/words").each_line do |line|
-      @words << line.chomp
+module Puppet; end
+module Puppet::Resource; end
+module Puppet::Transaction; end
+module Puppet::Util; end
+
+# http://projects.puppetlabs.com/projects/puppet/wiki/Report_Format_2
+class Puppet::Transaction::Report
+  attr_accessor :host, :time, :logs, :metrics, :resource_statuses,
+    :configuration_version, :report_format, :puppet_version, :kind, :status
+
+  def self.generate
+    Puppet::Transaction::Report.new.tap do |report|
+      report.host = DataGenerator.generate_hostname
+      report.time = DataGenerator.generate_time
+      report.logs = []
+      report.metrics = {}
+      report.resource_statuses = {}
+      report.configuration_version = Time.now.to_i
+      report.report_format = 2
+      report.puppet_version = "2.6.5"
+      report.kind = "apply"
+
+      3.times do
+        resource_status = DataGenerator.generate_resource_status(report)
+        report.resource_statuses[resource_status.resource] = resource_status
+      end
+
+      report.status = report.compute_status
     end
-    @words.delete_if {|word| word.length < 6 or word.squeeze != word or word.downcase != word}
   end
 
-  @domain ||= @words.random_element
-  @ext ||= ["net", "org", "com", "co.uk"].random_element
-
-  host = @domain
-  host = @words.random_element until host != @domain
-  "#{host}.#{@domain}.#{@ext}"
-end
-
-def generate_time
-  time = Time.now
-  time -= 1.day if rand(100) < 5
-  time.to_yaml.chomp.gsub('--- ','')
-end
-
-def generate_report
-  @report_template ||= File.read("report_template.yaml.erb")
-  hostname = generate_hostname
-  params = OpenStruct.new({
-    :configuration_version => Time.now.to_i,
-    :hostname => hostname,
-    :time => generate_time,
-  })
-  resources = []
-  3.times do
-    resources << generate_resource(params)
+  def compute_status
+    if resource_statuses.values.any? {|res| res.failed}
+      "failed"
+    elsif resource_statuses.values.any? {|res| res.changed}
+      "changed"
+    else
+      "unchanged"
+    end
   end
-  params.status = if resources.any? {|res| res.failed}
-                    "failed"
-                  elsif resources.any? {|res| res.change_count > 0}
-                    "changed"
-                  else
-                    "unchanged"
-                  end
-  params.resource_string = resources.map do |res|
-    ERB.new(@resource_template).result(res.send(:binding))
-  end.join.chomp
+end
+class Puppet::Util::Log
+  attr_accessor :file, :line, :level, :message, :source, :tags, :time
+end
+class Puppet::Util::Metric
+  attr_accessor :name, :label, :values
+end
+class Puppet::Resource::Status
+  attr_accessor :resource_type, :title, :resource, :file, :line,
+    :evaluation_time, :change_count, :out_of_sync_count, :tags, :time, :events,
+    :out_of_sync, :changed, :skipped, :failed
 
-  File.open("yaml/#{hostname}.yaml","w") do |f|
-    f.write ERB.new(@report_template).result(params.send(:binding))
+  def self.generate(report)
+    Puppet::Resource::Status.new.tap do |rs|
+      rs.resource_type     = "File"
+      rs.title             = File.join("/", (1..(rand(5)+2)).map {DataGenerator.generate_word})
+      rs.resource          = "#{rs.resource_type}[#{rs.title}]"
+      rs.file              = "/etc/puppet/manifests/site.pp"
+      rs.line              = rand(250) + 1
+      rs.evaluation_time   = Time.now.to_f % 1 + rand(3)
+      rs.change_count      = rand(100) < 80 ? 0 : rand(5)+1
+      rs.out_of_sync_count = rs.change_count
+      rs.tags              = ["node", "class", rs.resource_type.downcase, rs.title.downcase, report.host].sort
+      rs.time              = report.time
+      rs.events            = []
+      rs.out_of_sync       = rs.out_of_sync_count > 0
+      rs.changed           = rs.change_count > 0
+      rs.skipped           = false
+      rs.failed            = rs.change_count > 0 && rand(100) < 10
+    end
   end
-  #puts ERB.new(@report_template).result(params.send(:binding))
+end
+class Puppet::Transaction::Event
+  attr_accessor :audited, :property, :previous_value, :desired_value,
+    :historical_value, :message, :name, :status, :time
 end
 
-def generate_resource(report_params)
-  @resource_template = <<RESOURCE
-    "<%= resource_type %>[<%= title %>]": !ruby/object:Puppet::Resource::Status
-      change_count: <%= change_count %>
-      changed: <%= change_count > 0 %>
-      evaluation_time: <%= evaluation_time %>
-      events: []
-      failed: <%= failed %>
-      file: /etc/puppet/manifests/site.pp
-      line: <%= rand(250)+1 %>
-      out_of_sync: <%= out_of_sync_count > 0 %>
-      out_of_sync_count: <%= out_of_sync_count %>
-      resource: "<%= resource_type %>[<%= title %>]"
-      resource_type: <%= resource_type %>
-      skipped: false
-      tags: 
-        - <%= resource_type.downcase %>
-        - node
-        - <%= hostname %>
-        - class
-        - <%= title.downcase %>
-      time: <%= time %>
-      title: <%= title %>
-RESOURCE
+module DataGenerator
+  def self.generate_report
+    Puppet::Transaction::Report.generate
+  end
 
-  params = OpenStruct.new
-  params.resource_type = "File"
-  params.title = File.join("/", (1..(rand(5)+2)).map {@words.random_element})
-  params.change_count = rand(100) < 80 ? 0 : rand(5)+1
-  params.evaluation_time = Time.now.to_f % 1 + rand(3)
-  params.out_of_sync_count = params.change_count
-  params.hostname = report_params.hostname
-  params.time = report_params.time
-  params.failed = params.change_count > 0 && rand(100) < 10
+  def self.generate_resource_status(report)
+    Puppet::Resource::Status.generate(report)
+  end
 
-  params
+  def self.generate_hostname
+    @domain ||= generate_word
+    @ext ||= ["net", "org", "com", "co.uk"].random_element
+
+    host = @domain
+    host = generate_word until host != @domain
+    "#{host}.#{@domain}.#{@ext}"
+  end
+
+  def self.generate_time
+    time = Time.now
+    time -= 1.day if rand(100) < 5
+    time
+  end
+
+  def self.generate_word
+    words.random_element
+  end
+
+  private
+
+  def self.words
+    if @words.nil?
+      @words = []
+      File.open("/usr/share/dict/words").each_line do |line|
+        @words << line.chomp
+      end
+      @words.delete_if {|word| word.length < 6 or word.squeeze != word or word.downcase != word}
+    end
+    @words
+  end
 end
--- populate_dashboard.rb
+++ populate_dashboard.rb
@@ -3,21 +3,19 @@
 require 'generate_report'
 require 'rubygems'
 
-unless ARGV.length > 0
-  puts "Specify the rakefile to use"
-  exit 1
+rakefile_if_specified = "-f #{ARGV.first}" unless ARGV.empty?
+
+#100.times do
+report = DataGenerator.generate_report
+File.open("yaml/#{report.host}.yaml","w") do |f|
+	f.print report.to_yaml
 end
-
-rakefile = ARGV.first
-
-100.times do
- generate_report
-end
-
-puts "Importing reports"
-`rake -f #{rakefile} reports:import REPORT_DIR=yaml`
-
-10.times do
-  puts `rake -f #{rakefile} node:add name=#{generate_hostname}`
-end
+#end
+#
+#puts "Importing reports"
+puts "rake #{rakefile_if_specified} reports:import REPORT_DIR=yaml"
+#
+#10.times do
+#  puts `rake #{rakefile_if_specified} node:add name=#{generate_hostname}`
+#end
 
